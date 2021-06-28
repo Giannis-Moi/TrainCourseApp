a03d27f82ffcf6706d60edcd0b0aedca9f976a30
Giannis-Moi <85611864+Giannis-Moi@users.noreply.github.com>
2021-06-28 16:24:12 +0300

    Apply Migration

---
+++ TrainCourseApp.EF/Context/TrainCourseContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainCourseApp.EF.Context {
+    class TrainCourseContextFactory : IDesignTimeDbContextFactory<TrainCourseContext> {
+        public TrainCourseContext CreateDbContext(string[] args) {
+
+            var optionsBuilder = new DbContextOptionsBuilder<TrainCourseContext>();
+
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-B2H5QAB\SQLEXPRESS;Initial Catalog=DbTrainCourse;Trusted_Connection=True;");
+
+            return new TrainCourseContext(optionsBuilder.Options);
+
+        }
+    }
+}
---
+++ TrainCourseApp.EF/Migrations/20210628130627_InitialCreate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrainCourseApp.EF.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "School");
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                schema: "School",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Student",
+                schema: "School");
+        }
+    }
+}
